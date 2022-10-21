23ecf6c5c521e5b6e234395d621fa72c8f15071d
Boris Gaidukevich <Borishahn@yandex.ru>
2022-10-22 02:22:33 +0300

    0.1.5

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,9 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
 const User = require('../models/user');
+
 const {
   created, badRequest, notFound, internalServerError,
 } = require('../utils/errors');
@@ -32,8 +37,21 @@ module.exports.getUserById = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
+  const {
+    name,
+    about,
+    avatar,
+    email,
+    password,
+  } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    }))
     .then((user) => res.status(created).send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
@@ -89,5 +107,28 @@ module.exports.editAvatar = (req, res) => {
         return;
       }
       res.status(internalServerError).send({ message: 'Произошла ошибка' });
+    });
+};
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+  User.findUser({ email, password })
+    .then((user) => {
+      const token = jwt.sign(
+        { _id: user._id },
+        JWT_SECRET,
+        { expiresIn: '7d' },
+      );
+      res
+        .cookie('jwt', token, {
+          maxAge: 3600000 * 24 * 7,
+          httpOnly: true,
+        })
+        .end();
+    })
+    .catch((err) => {
+      res
+        .status(401)
+        .send({ message: err.message });
     });
 };
