2e176d927d01f532e92057a92c50819cf27250dd
Boris Gaidukevich <Borishahn@yandex.ru>
2022-10-25 01:24:28 +0300

    0.1.9

--- models/user.js
+++ models/user.js
@@ -2,44 +2,39 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      default: 'Жак-Ив Кусто',
-      minLength: 2,
-      maxLength: 30,
-    },
-    about: {
-      type: String,
-      default: 'Исследователь',
-      minLength: 2,
-      maxLength: 30,
-    },
-    avatar: {
-      type: String,
-      default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-      validator: (value) => validator.isURL(value, { protocols: ['http', 'https'], require_protocol: true }),
-      message: 'Некорректный URL',
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      validator: (value) => validator.isEmail(value),
-      message: ({ value }) => `${value} - Некорректный email`,
-    },
-    password: {
-      type: String,
-      required: true,
-      minLength: 8,
-      select: false,
-    },
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    default: 'Жак-Ив Кусто',
+    minLength: 2,
+    maxLength: 30,
   },
-  {
-    versionKey: false,
+  about: {
+    type: String,
+    default: 'Исследователь',
+    minLength: 2,
+    maxLength: 30,
   },
-);
+  avatar: {
+    type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validator: (value) => validator.isURL(value, { protocols: ['http', 'https'], require_protocol: true }),
+    message: 'Некорректный URL',
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validator: (value) => validator.isEmail(value),
+    message: ({ value }) => `${value} - Некорректный email`,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+    select: false,
+  },
+}, { toObject: { useProjection: true }, toJSON: { useProjection: true } });
 
 userSchema.statics.findUser = function (email, password) {
   return this.findOne({ email })
